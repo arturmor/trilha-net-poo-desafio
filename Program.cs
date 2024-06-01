adb66cf30309785ff86990dea69de923cd56da9f
Artur Moraes <brito.artur97@gmail.com>
2024-06-01 11:36:21 -0300

    Desafio resolvido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste Nokia:");
+            Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789", 16);
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            Console.WriteLine("\nTeste Iphone:");
+            Iphone iphone = new Iphone("987654321", "Iphone 12", "987654321", 128);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+        }
+    }
+}
